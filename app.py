6d56b26159ef990e36d7524c1b1952f08fb09849
Vibishna Baskaran <110372617+Vibishna17@users.noreply.github.com>
2023-11-07 20:51:16 +0530

    Update app.py
    summary is added extra

--- app.py
+++ app.py
@@ -1,8 +1,46 @@
+
 from flask import Flask, render_template, request
 from imdb import IMDb
 from textblob import TextBlob
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize
+import heapq
 
 app = Flask(__name__)
+
+# Define the nltk_summarizer function for text summarization
+def nltk_summarizer(raw_text):
+    stopWords = set(stopwords.words("english"))
+    word_frequencies = {}
+    for word in nltk.word_tokenize(raw_text):
+        if word not in stopWords:
+            if word not in word_frequencies.keys():
+                word_frequencies[word] = 1
+            else:
+                word_frequencies[word] += 1
+
+    maximum_frequency = max(word_frequencies.values())
+
+    for word in word_frequencies.keys():
+        word_frequencies[word] = (word_frequencies[word] / maximum_frequency)
+
+    sentence_list = nltk.sent_tokenize(raw_text)
+    sentence_scores = {}
+    for sent in sentence_list:
+        for word in nltk.word_tokenize(sent.lower()):
+            if word in word_frequencies.keys():
+                if len(sent.split(' ')) < 30:
+                    if sent not in sentence_scores.keys():
+                        sentence_scores[sent] = word_frequencies[word]
+                    else:
+                        sentence_scores[sent] += word_frequencies[word]
+
+    summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
+    summary = ' '.join(summary_sentences)
+    return summary
 
 def get_movie_info(movie_title):
     ia = IMDb()
@@ -14,13 +52,23 @@ def get_movie_info(movie_title):
     movie = movies[0]
     ia.update(movie, ["reviews"])
     
-    reviews = [{"content": review["content"], "sentiment": analyze_sentiment(review["content"])} for review in movie["reviews"]]
+    reviews = []
+    for review in movie["reviews"]:
+        review_text = review["content"]
+        summary = nltk_summarizer(review_text)  # Summarize the review
+        sentiment = analyze_sentiment(review_text)
+        reviews.append({"content": review_text, "summary": summary, "sentiment": sentiment})
+    
+    
     rating = movie.data.get("rating", "N/A")
+
     overall_sentiment = analyze_overall_sentiment(reviews)
 
     positive_count, neutral_count, negative_count = count_sentiments(reviews)
 
     return reviews, rating, overall_sentiment, positive_count, neutral_count, negative_count
+
+# Rest of your code remains unchanged
 
 def analyze_sentiment(review):
     polarity_score = TextBlob(review).sentiment.polarity
